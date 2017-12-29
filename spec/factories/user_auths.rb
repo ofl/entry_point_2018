0f88e2882f5b041929a3d5378f971ebe8ff487f5
ofl <ofl@covered.jp>
2017-12-29 11:00:36 +0900

    [add]spec/models/user_spec

---
+++ spec/factories/user_auths.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: user_auths
+#
+#  id                   :integer          not null, primary key
+#  user_id              :integer          not null
+#  provider             :integer          default("email"), not null
+#  uid                  :string           not null
+#  access_token         :string
+#  access_secret        :string
+#  confirmation_token   :string
+#  confirmed_at         :datetime
+#  confirmation_sent_at :datetime
+#
+# Indexes
+#
+#  index_user_auths_on_confirmation_token  (confirmation_token) UNIQUE
+#  index_user_auths_on_provider_and_uid    (provider,uid) UNIQUE
+#  index_user_auths_on_user_id             (user_id)
+#
+
+FactoryBot.define do
+  factory :user_auth do
+    user
+    provider { :facebook }
+    uid { SecureRandom.uuid }
+
+    factory :email_user_auth do
+      provider { :email }
+      uid { 'foo@example.com' }
+    end
+  end
+end
