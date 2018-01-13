ea3464cc9c5844565892782abc94f68e387e6fe7
ofl <ofl@covered.jp>
2018-01-13 11:34:23 +0900

    [update]spec/request/users/registrations_spec.rb

--- spec/request/users/registrations_spec.rb
+++ spec/request/users/registrations_spec.rb
@@ -27,17 +27,25 @@ RSpec.describe 'Users::Registrations', type: :request do
       before { sign_in user }
 
       it { is_expected.to redirect_to authenticated_root_path }
+      it do
+        expect { subject }.not_to change(User, :count)
+      end
     end
 
     context 'not logged in' do
       context 'invalid params' do
         let(:params) { invalid_params }
+
         it { is_expected.to eq 200 }
       end
 
       context 'valid params' do
         let(:params) { valid_params }
+
         it { is_expected.to redirect_to authenticated_root_path }
+        it do
+          expect { subject }.to change(User, :count).by(1)
+        end
       end
     end
   end
