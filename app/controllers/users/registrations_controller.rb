eeceb69a1a447cd5ca34de1de885d3c0ecb8a104
ofl <ofl@covered.jp>
2018-02-03 14:45:35 +0900

    [add]Users::RegistrationsController#create_user_auth

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,31 +11,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     build_resource(sign_up_params)
 
-    resource.save
-
-    if params[:preview]
-      unless resource.valid?
-        clean_up_passwords resource
-        set_minimum_password_length
-      end
+    case registration_action
+    when :back
       render :new
-      return
-    end
-
-    if resource.persisted?
-      if resource.active_for_authentication?
-        set_flash_message! :notice, :signed_up
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
-      else
-        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
-      end
+    when :preview
+      preview
     else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
+      register_user_and_create_user_auth
     end
   end
 
@@ -72,5 +54,63 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def after_update_path_for(_resource)
     authenticated_root_path
+  end
+
+  def preview
+    unless resource.valid?
+      clean_up_passwords resource
+      set_minimum_password_length
+    end
+    render :new
+  end
+
+  def registration_action
+    @registration_action ||= %i(back preview email twitter facebook).find do |action_name|
+      params[action_name].present?
+    end
+  end
+
+  def register_user_and_create_user_auth
+    resource.save
+    if resource.persisted?
+      set_flash_message! :notice, :signed_up
+      sign_up(resource_name, resource)
+
+      user_auth = create_user_auth
+      if user_auth.persisted?
+        if user_auth.external_auth_provider?
+          redirect_to_external_auth_provider(user_auth)
+        else
+          send_confrimation_mail(user_auth)
+          set_flash_message! :notice, :confirmation_mail_sent
+          redirect_to after_sign_up_path_for(resource)
+        end
+      else
+        set_flash_message! :notice, :failed_create_user_auth
+        redirect_to after_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
+  def create_user_auth
+    user_auth = resource.user_auths.find_or_initialize_by(provider: registration_action)
+    user_auth.uid = resource.email if registration_action == :email
+    user_auth.add_confirmation_token
+    user_auth.save
+    user_auth
+  end
+
+  def redirect_to_external_auth_provider(user_auth)
+    redirect_to send(
+      "user_#{user_auth.provider}_omniauth_authorize_path", connect: user_auth.id
+    )
+  end
+
+  def send_confrimation_mail(user_auth)
+    AuthenticationMailer.confirmation_instructions(resource, user_auth).deliver_now
   end
 end
