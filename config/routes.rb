92810c0c08e21044a27d8d20a443a8e2e1f84d92
ofl <ofl@covered.jp>
2018-02-16 09:50:09 +0900

    [remove]ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
     root to: 'mypage#show', as: :authenticated_root
   end
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
