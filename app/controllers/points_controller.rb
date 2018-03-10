28c970d327024dd901cbd46ca5b5cd524bd1ee6f
ofl <ofl@covered.jp>
2018-03-10 16:11:20 +0900

    [update]PointsController#index, show, create

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,11 +1,16 @@
 class PointsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @points = current_user.points.order(created_at: :desc)
   end
 
   def show
+    @point = current_user.points.find(params[:id])
   end
 
   def create
-
+    current_user.points.create(status: :got, amount: rand(1..100))
+    redirect_to points_path
   end
 end
