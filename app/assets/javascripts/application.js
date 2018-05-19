fd1044070f75a90d4c45cbf9d30e063c2dbbb74c
ofl <ofl@covered.jp>
2018-05-19 12:26:54 +0900

    [#133]ログイン画面の修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
 //= require jquery.turbolinks
-//= require materialize-sprockets
+//= require popper
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
