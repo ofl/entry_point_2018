46b71cdd0446853f146d026e6c6cb1aeb709afc6
ofl <ofl@covered.jp>
2018-05-26 11:38:52 +0900

    [#133]Deviseのエラーメッセージを表示できるようにする

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
 module DeviseHelper
-  def devise_error_messages!
-    return '' unless devise_error_messages?
+  def devise_error_messages! # rubocop:disable Metrics/MethodLength
+    return '' if resource.errors.empty?
 
-    err = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     html = <<-HTML
-    <ul>#{err}</ul>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+      <button type="button" class="close" data-dismiss="alert">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      <strong>
+       #{pluralize(resource.errors.count, 'error')} must be fixed
+      </strong>
+      #{messages}
+    </div>
     HTML
 
     html.html_safe # rubocop:disable Rails/OutputSafety
