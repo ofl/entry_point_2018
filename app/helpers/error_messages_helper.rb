fc54fb5548fe7a47770aceceb1fd03f89d2ba482
ofl <ofl@covered.jp>
2018-05-27 15:40:31 +0900

    [#133]フォームのエラー表示

--- app/helpers/error_messages_helper.rb
+++ app/helpers/error_messages_helper.rb
@@ -2,18 +2,17 @@
 
 module ErrorMessagesHelper
   def error_messages!(object)
-    return '' unless error_messages?(object)
+    return '' unless object.errors.any?
 
     err = object.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
 
     html = <<-HTML
-    <ul>#{err}</ul>
+    <div class="alert alert-danger">
+      <a href="#" class="close" data-dismiss="alert">×</a>
+      <ul>#{err}</ul>
+    </div>
     HTML
 
     html.html_safe # rubocop:disable Rails/OutputSafety
-  end
-
-  def error_messages?(object)
-    !object.errors.empty?
   end
 end
