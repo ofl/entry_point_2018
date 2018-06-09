1b95964eea26aa20ba2384a84a746435081b220b
ofl <ofl@covered.jp>
2018-06-09 15:02:39 +0900

    [#139]`rails webpacker:install:vue`

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,44 +1,10 @@
 /* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
 
-import '../src/application'
-require.context('../images', true, /\.(png|jpg|jpeg|svg)$/)
-
-import 'bootstrap/dist/js/bootstrap'
-
-import Rails from 'rails-ujs';
-Rails.start();
-
-import Turbolinks from 'turbolinks'
-Turbolinks.start()
-
-import Vue from 'vue'
-import TurbolinksAdapter from 'vue-turbolinks'
-Vue.use(TurbolinksAdapter)
-
-// see https://qiita.com/midnightSuyama/items/efc5441a577f3d3abe74
-
-var vms = []
-var options = {}
-
-var requireContext = require.context('./options', false, /\.js$/)
-requireContext.keys().forEach(key => {
-  let name = key.split('/').pop().split('.').shift()
-  options[name] = requireContext(key).default
-})
-
-document.addEventListener('turbolinks:load', () => {
-  let templates = document.querySelectorAll('[data-vue]')
-  for (let el of templates) {
-    let vm = new Vue(
-      Object.assign(options[el.dataset.vue], { el })
-    )
-    vms.push(vm)
-  }
-})
-
-document.addEventListener('turbolinks:visit', () => {
-  for (let vm of vms) {
-    vm.$destroy()
-  }
-  vms = []
-})
+console.log('Hello World from Webpacker')
