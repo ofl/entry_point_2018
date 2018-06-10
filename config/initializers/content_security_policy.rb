0283f69e300e9a3aa13567ef6b4680c7c8a51e8b
ofl <ofl@covered.jp>
2018-06-10 13:28:50 +0900

    [#139]webpacker-dev-serverのportを8080から3035に修正

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -16,7 +16,7 @@ Rails.application.config.content_security_policy do |policy|
   #   # policy.report_uri "/csp-violation-report-endpoint"
 
   if Rails.env.development?
-    policy.script_src :self, :https, :unsafe_eval, 'http://0.0.0.0:8080', 'ws://0.0.0.0:8080'
+    policy.script_src :self, :https, :unsafe_eval
   else
     policy.script_src :self, :https
   end
