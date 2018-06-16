11bf03fe51fd09e7de2a61e22df79e66dc5dad5f
ofl <ofl@covered.jp>
2018-06-16 17:26:56 +0900

    [#141]`rails generate graphql:install`

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
   authenticated :user do
     root to: 'mypage#show', as: :authenticated_root
 
