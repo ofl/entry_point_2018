9baa897c0c3943a4b280c095b02214e19046e535
ofl <ofl@covered.jp>
2018-06-30 15:14:56 +0900

    [#141]Types::QueryTypeにfield :postを追加

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,11 +10,20 @@ class Types::QueryType < Types::BaseObject
     description 'Find current user'
   end
 
+  field :post, PostType, null: true do
+    description 'IDによる取得'
+    argument :id, ID, required: true
+  end
+
   def user(username:)
     User.find_by(username: username)
   end
 
   def current_user
     context[:current_user]
+  end
+
+  def post(id:)
+    Post.find(id)
   end
 end
