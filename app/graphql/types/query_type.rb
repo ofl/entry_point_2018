39216b5a14381184d9b7879cc0691d5ca4abcf62
ofl <ofl@covered.jp>
2018-07-07 17:59:52 +0900

    [#141]GraphQL: コメントの投稿機能のテストの追加

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,6 +3,7 @@ class Types::QueryType < Types::BaseObject
 
   field :user,         resolver: Queries::Users::Show
   field :current_user, resolver: Queries::Users::CurrentUser
+
   field :posts,        resolver: Queries::Posts::Index
   field :post,         resolver: Queries::Posts::Show
 end
