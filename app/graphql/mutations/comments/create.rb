2f1bf32faf732f2189f42fce39defe08e2461665
ofl <ofl@covered.jp>
2018-07-16 18:22:06 +0900

    [#141]入力値をInput Objectsに変更

--- app/graphql/mutations/comments/create.rb
+++ app/graphql/mutations/comments/create.rb
@@ -4,21 +4,14 @@ class Mutations::Comments::Create < Mutations::BaseMutation
 
   null true
 
-  argument :body, String, description: 'コメントの本文', required: true
-  argument :post_id, ID, description: 'コメントを追加する投稿ID', required: true
+  argument :attributes, Types::CreateCommentAttributes, required: true
 
   field :comment, Types::CommentType, null: true
 
-  def resolve(body:, post_id:)
-    # HACK: 認証状態の確認。メソッドごとにcheck_authorization_statusを書くのは冗長なので
-    check_authorization_status
-
-    post = Post.find_by(id: post_id)
-    { comment: context[:current_user].comments.create!(post: post, body: body) }
-  rescue ActiveRecord::RecordInvalid => e
-    error_type = ErrorTypes::BadRequest.new(e)
-    Rails.logger.error error_type.logs.join("\n")
-
-    convert_to_bad_request_errors(error_type, e.record.errors)
+  def resolve(attributes:)
+    check_authorization_and_excute do
+      comment = context[:current_user].comments.create!(attributes.to_h)
+      { comment: comment }
+    end
   end
 end
